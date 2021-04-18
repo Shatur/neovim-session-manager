a84a628c95be97bc4462937953332199e35337b5
Shatur95 <genaloner@gmail.com>
2021-04-19 01:35:06 +0300

    Clean previous session correctly

--- lua/session_manager/init.lua
+++ lua/session_manager/init.lua
@@ -2,10 +2,20 @@ local utils = require('session_manager.utils')
 local session_manager = {}
 
 function session_manager.load_session(session_filename, save_current)
+  -- Remove all non-file buffers first
+  for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+    if #vim.api.nvim_buf_get_option(buffer, 'buftype') ~= 0 then
+      -- Use vimscript API because vim.api.nvim_buf_delete causes issues with Lua callbacks
+      vim.cmd('bdelete! ' .. tostring(buffer))
+    end
+  end
+
+  -- Stop all LSP clients
+  vim.lsp.stop_client(vim.lsp.get_active_clients())
+
   if save_current then
     session_manager.save_session()
   end
-
 
   -- Load last session
   if not session_filename then
@@ -16,10 +26,7 @@ function session_manager.load_session(session_filename, save_current)
     session_filename = vim.g.sessions_dir .. last_session.filename
   end
 
-  -- Remove all buffers
-  for _, handle in ipairs(vim.api.nvim_list_bufs()) do
-    vim.api.nvim_buf_delete(handle, {force = #vim.api.nvim_buf_get_option(handle, 'buftype') ~= 0})
-  end
+  vim.cmd('bufdo bdelete') -- Remove all buffers
 
   vim.cmd('source ' .. session_filename)
 end
