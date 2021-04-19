304684dc6efa3b61a46786e13fbef0d72618624e
Shatur95 <genaloner@gmail.com>
2021-04-20 00:03:17 +0300

    Correctly cleanup previous session

--- lua/session_manager/init.lua
+++ lua/session_manager/init.lua
@@ -2,17 +2,6 @@ local utils = require('session_manager.utils')
 local session_manager = {}
 
 function session_manager.load_session(session_filename, save_current)
-  -- Remove all non-file buffers first
-  for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
-    if #vim.api.nvim_buf_get_option(buffer, 'buftype') ~= 0 or vim.api.nvim_buf_get_option(buffer, 'buflisted') == 0 then
-      -- Use vimscript API because vim.api.nvim_buf_delete causes issues with Lua callbacks
-      vim.cmd('bdelete! ' .. tostring(buffer))
-    end
-  end
-
-  -- Stop all LSP clients
-  vim.lsp.stop_client(vim.lsp.get_active_clients())
-
   if save_current then
     session_manager.save_session()
   end
@@ -26,15 +15,35 @@ function session_manager.load_session(session_filename, save_current)
     session_filename = vim.g.sessions_dir .. last_session.filename
   end
 
-  vim.cmd('silent bufdo bdelete') -- Remove all buffers
+  -- Stop all LSP clients first
+  vim.lsp.stop_client(vim.lsp.get_active_clients())
 
-  vim.cmd('silent source ' .. session_filename)
+  -- Scedule buffers cleanup to avoid callback issues and source the session
+  vim.schedule(function()
+    vim.cmd('bufdo! bwipeout') -- Remove all buffers
+    vim.cmd('silent source ' .. session_filename)
+  end)
 end
 
 function session_manager.save_session()
   if vim.fn.isdirectory(vim.g.sessions_dir) ~= 1 then
     vim.fn.mkdir(vim.g.sessions_dir)
   end
+
+  -- Remove all non-file and utility buffers because they cannot be saved
+  for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buffer) then
+      if #vim.api.nvim_buf_get_option(buffer, 'buftype') ~= 0 or vim.api.nvim_buf_get_option(buffer, 'buflisted') == 0 then
+        vim.api.nvim_buf_delete(buffer, {force = true, unload = true})
+      end
+    end
+  end
+
+  -- Clear all passed arguments to avoid re-executing them
+  if vim.fn.argc() > 0 then
+    vim.cmd('%argdel')
+  end
+
   vim.cmd('mksession! ' .. vim.g.sessions_dir .. utils.path_to_session_name(vim.fn.getcwd()))
 end
 
