bdd47d4892279e53097e84cd47ca29d87ee70428
Shatur95 <genaloner@gmail.com>
2021-04-22 18:19:05 +0300

    Check if project directory exists

--- lua/session_manager/utils.lua
+++ lua/session_manager/utils.lua
@@ -29,7 +29,11 @@ end
 function utils.get_sessions()
   local sessions = {}
   for _, session_filename in ipairs(vim.fn.readdir(vim.g.sessions_dir)) do
-    table.insert(sessions, {timestamp = vim.fn.getftime(vim.g.sessions_dir .. session_filename), filename = session_filename})
+    if vim.fn.isdirectory(utils.session_name_to_path(session_filename)) == 1 then
+      table.insert(sessions, {timestamp = vim.fn.getftime(vim.g.sessions_dir .. session_filename), filename = session_filename})
+    else
+      vim.fn.delete(session_filename)
+    end
   end
   table.sort(sessions, function(a, b) return a.timestamp > b.timestamp end)
 
@@ -44,10 +48,12 @@ end
 function utils.get_last_session()
   local most_recent_session = {timestamp = 0, filename = nil}
   for _, session_filename in ipairs(vim.fn.readdir(vim.g.sessions_dir)) do
-    local timestamp = vim.fn.getftime(vim.g.sessions_dir .. session_filename)
-    if most_recent_session.timestamp < timestamp then
-      most_recent_session.timestamp = timestamp
-      most_recent_session.filename = session_filename
+    if vim.fn.isdirectory(utils.session_name_to_path(session_filename)) == 1 then
+      local timestamp = vim.fn.getftime(vim.g.sessions_dir .. session_filename)
+      if most_recent_session.timestamp < timestamp then
+        most_recent_session.timestamp = timestamp
+        most_recent_session.filename = session_filename
+      end
     end
   end
   return most_recent_session
