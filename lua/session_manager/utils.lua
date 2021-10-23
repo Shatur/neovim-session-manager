9ce34c0c35ebf9af9998e3abe51733bbd0cbc5d2
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-10-23 20:06:31 +0300

    Reuse is_normal_buffer

--- lua/session_manager/utils.lua
+++ lua/session_manager/utils.lua
@@ -66,7 +66,7 @@ function utils.save_session(filename)
   -- Remove all non-file and utility buffers because they cannot be saved
   for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
     if vim.api.nvim_buf_is_valid(buffer) then
-      if #vim.api.nvim_buf_get_option(buffer, 'buftype') ~= 0 or not vim.api.nvim_buf_get_option(buffer, 'buflisted') then
+      if not utils.is_normal_buffer(buffer) then
         vim.api.nvim_buf_delete(buffer, { force = true })
       end
     end
