6a2aee9ece9ce9fdbaafeb1bc693d3a21cab8825
Yajie Zhu <15101230@qq.com>
2022-09-26 00:03:31 +0800

    Close unused LSP clients

--- lua/session_manager/utils.lua
+++ lua/session_manager/utils.lua
@@ -3,6 +3,27 @@ local scandir = require('plenary.scandir')
 local Path = require('plenary.path')
 local utils = { is_session = false }
 
+
+-- close unused lsp clients
+local function close_unused_lsp_clients()
+  local bufs = vim.api.nvim_list_bufs()
+  local lsp_clients = vim.lsp.get_active_clients()
+  for _, client in pairs(lsp_clients) do
+    if client.config.root_dir ~= vim.loop.cwd() then
+      local active = false
+      for _, bufnr in pairs(bufs) do
+        if client.attached_buffers[bufnr] ~= nil then
+          active = true
+          break
+        end
+      end
+      if active == false then
+        vim.lsp.stop_client(client.id)
+      end
+    end
+  end
+end
+
 --- A small wrapper around `vim.notify` that adds plugin title.
 ---@param msg string
 ---@param log_level number
@@ -61,6 +82,7 @@ function utils.load_session(filename, discard_current)
     utils.is_session = true
     vim.api.nvim_exec_autocmds('User', { pattern = 'SessionLoadPre' })
     vim.api.nvim_command('silent source ' .. filename)
+    close_unused_lsp_clients()
     vim.api.nvim_exec_autocmds('User', { pattern = 'SessionLoadPost' })
   end)
 end
