d7a5e3d469bb94f108bba8d2fc48d2f8632e6eb8
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-06-27 23:19:38 +0300

    Revert "Fix loading session error if swap file warning printed (#83)"
    This reverts commit bb47cab66c385bd76e561365b361b3478cffcee4.

--- lua/session_manager/utils.lua
+++ lua/session_manager/utils.lua
@@ -79,7 +79,7 @@ function utils.load_session(filename, discard_current)
 
     utils.is_session = true
     vim.api.nvim_exec_autocmds('User', { pattern = 'SessionLoadPre' })
-    vim.api.nvim_command('silent! source ' .. filename)
+    vim.api.nvim_command('silent source ' .. filename)
     vim.api.nvim_exec_autocmds('User', { pattern = 'SessionLoadPost' })
     close_unused_lsp_clients()
   end)
